da8cf5ad769d648d2f4205a344c957efed3000ce
Hannah Mowbray <hannahmowbray3@gmail.com>
2025-03-04 14:11:04 +0000

    stop sound at end
    Signed-off-by: Hannah Mowbray <hannahmowbray3@gmail.com>

--- src/components/Journey/Feedback.jsx
+++ src/components/Journey/Feedback.jsx
@@ -24,7 +24,7 @@ export const Feedback = () => {
   const [playLose] = useSound(lose);
 
   useEffect(() => {
-    add ? playWin() : playLose();
+    !isLastStep && add ? playWin() : playLose();
   }, [playWin]);
 
   const isLastStep = step === journeySteps.length - 1;
